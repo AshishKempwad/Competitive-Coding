407d7dddf6ef0bba5c03dcefe0cd777d173c0346
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-03-23 03:50:54 +0530

    Bit Manipulation

---
+++ 7)FindOddOrEven.cpp
@@ -0,0 +1,15 @@
+int findOddorEven(int n)
+{
+  //Will learn the optimized way of finding if a number is even or odd using a bit wise & operator. 
+  
+   if((n & 1)==0)
+  {
+    cout<<"Even";
+  }
+else
+{
+cout<<"Odd";
+}
+
+
+}
