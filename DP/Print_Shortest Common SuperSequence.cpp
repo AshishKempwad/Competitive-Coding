c924a40b39325cbfd5c35068760e89135352356f
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-07-03 23:29:08 +0530

    LCS is the parent problem
    Prereq :
     SCS and print LCS

---
+++ DP/Print_Shortest Common SuperSequence.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2)
+    {
+        int n= str1.size();
+        int m = str2.size();
+        
+        int dp[n+1][m+1];
+        
+        for(int i=0;i<=n;i++)    //Initialize
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<=n;i++)    //LCS
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(str1[i-1]==str2[j-1])
+                {
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        int i=n;
+        int j=m;
+        
+        string res = "";
+        
+        while(i>0 && j>0)
+        {
+            if(str1[i-1]==str2[j-1])
+            {
+               
+                res.push_back(str1[i-1]);
+                i--;
+                j--;
+            }
+            else
+            {
+                if(dp[i][j-1] > dp[i-1][j])
+                {
+                    res.push_back(str2[j-1]);
+                    j--;
+                }
+                else
+                {
+                     res.push_back(str1[i-1]);
+                    i--;
+                }
+            }
+        }
+        
+        while(i>0)
+        {
+            res.push_back(str1[i-1]);
+            i--;
+        }
+        
+        while(j>0)
+        {
+           res.push_back(str2[j-1]);
+            j--;
+        }
+        
+        reverse(res.begin(),res.end());
+        return res;
+        
+    }
+};
