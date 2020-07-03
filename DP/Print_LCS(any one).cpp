75ceba0fcb63cff356f4d00fd27343196a3ac0b2
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-07-03 18:50:29 +0530

    LCS is Parent problem

---
+++ DP/Print_LCS(any one).cpp
@@ -0,0 +1,63 @@
+vector<int> longestCommonSubsequence(vector<int> a, vector<int> b) 
+{
+    int n = a.size();
+    int m = b.size();
+
+    int dp[n+1][m+1];
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(a[i-1]==b[j-1])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j] = max( dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+
+    int i=n;
+    int j=m;
+    vector<int>res;
+    while(i>0 || j>0)
+    {
+        if(a[i-1]==b[j-1])
+        {
+            int ans = a[i-1];
+            res.push_back(ans);
+            i--;
+            j--;
+        }
+        else
+        {
+            if(dp[i][j-1] > dp[i-1][j])
+            {  
+                j--;
+            }
+            else
+            {
+                i--;
+            }
+        }
+    }
+
+    reverse(res.begin(),res.end());
+    return res;
+    
+
+}
