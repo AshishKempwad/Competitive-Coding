74e34b9b4d348f7b145555ffde400bd94d5ecfe3
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-07-13 14:57:01 +0530

    This is parent problem

---
+++ Stack/Next_greater_element_to_right.cpp
@@ -0,0 +1,21 @@
+vector <long long> nextLargerElement(long long arr[], int n)
+{
+    vector<long long> ans;
+    stack<long long> s;
+    
+    for(int i=n-1;i>=0;i--)
+    {
+          while(!s.empty() && arr[i] >= s.top())
+          {
+	            s.pop();
+	        }
+	        if(s.empty())
+	            ans.push_back(-1);
+	        else
+	            ans.push_back(s.top());
+	        s.push(arr[i]);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+    // Your code here
+}
