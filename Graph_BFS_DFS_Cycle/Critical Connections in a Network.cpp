a5ac76b8b0385585c0463c35697a93c18ccaf26d
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-09-18 17:31:13 +0530

    Tarjan's Algorithm

---
+++ Graph_BFS_DFS_Cycle/Critical Connections in a Network.cpp
@@ -0,0 +1,57 @@
+
+class Solution {
+public:
+    int timer=0;
+vector<vector<int>>ans;
+
+void dfs(int node, int par, vector<bool>&vis, vector<int>&id, vector<int>&low_link,vector<vector<int>>&graph)
+{
+    vis[node]=true;
+    id[node] = low_link[node]=timer++;
+   
+    
+    for(auto x : graph[node])
+    {
+        if(x != par)
+        {
+            if(!vis[x])
+            {
+                dfs(x,node,vis,id,low_link,graph);
+                low_link[node] = min(low_link[node], low_link[x]);
+                if(id[node] < low_link[x])
+                {
+                    ans.push_back({node,x});
+                }
+            }
+            else
+            {
+                low_link[node] = min(low_link[node],low_link[x]);
+            }
+        }
+    }
+}
+     
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections)
+    {
+        vector<vector<int>>graph(n+1);
+        for(auto &x : connections)
+        {
+            graph[x[0]].push_back(x[1]);
+            graph[x[1]].push_back(x[0]);
+        }
+        
+        vector<bool>vis(n+1,false);
+        vector<int>id(n+1,0);
+        vector<int>low_link(n+1,0);
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                dfs(i,i,vis,id,low_link,graph);
+            }
+        }
+        return ans;
+    }
+};
