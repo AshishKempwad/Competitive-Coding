4b2d328c7cbd210d66732e6085f4652767d68880
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-09-23 17:34:25 +0530

    Use the knowledge of "231"

---
+++ Trees/Valid BST from preorder.cpp
@@ -0,0 +1,39 @@
+int Solution::solve(vector<int> &A)  //Preorder traversal has been given. 
+{
+    //Preorder traversal can NEVER be in the format "2-3-1"
+    // Format(represents) 2(root)-3(left)-1(right).ie. root can never be less than right element.
+    //We use this and check if "231" pattern is present in the list. If present, then we can
+    //say that it is NOT a BST.
+    //WE USE STACK FOR THIS PURPOSE
+    
+
+    //PREORDER STRUCTURE : (ROOT-LEFT-RIGHT)
+    
+    
+    if(A.size() < 1) //Empty tree is a BST
+    {
+        return 1;
+    }
+    
+    stack<int>s;
+    
+   int root = INT_MIN;
+    
+    for(int i=0;i<A.size();i++)
+    {
+        if(root > A[i])   //As root can never be less than right element in BST
+        {
+            return 0;
+        }
+        
+        while(!s.empty() && A[i] > s.top())
+        {
+            root = s.top();   //update the value of root
+            s.pop();
+        }
+        
+        s.push(A[i]);
+    }
+    
+    return 1;  // If we dont find "231" pattern then we can say that it is a BST
+}
