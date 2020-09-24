6239996c964ee68fe3d2b2b443f7c3bdccf993f8
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-09-24 13:39:23 +0530

    Create Highest Product.cpp

---
+++ Greedy/Highest Product.cpp
@@ -0,0 +1,10 @@
+int Solution::maxp3(vector<int> &A)
+{ 
+    int n = A.size();
+    if(n < 3) {
+        return 0;
+    }
+    sort(A.begin(), A.end());
+    return max(A[0]*A[1]*A[n-1], A[n-1]* A[n-2]*A[n-3]);  //list might contain negative numbers as well
+    //We find biggest negative numbers in the beginning and product of two negative is positive
+}
