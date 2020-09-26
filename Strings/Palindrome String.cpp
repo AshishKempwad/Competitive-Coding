733be0c307c9456a2d389a4e21525866e3307c31
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-09-26 11:57:06 +0530

    Create  Palindrome String.cpp

---
+++ Strings/Palindrome String.cpp
@@ -0,0 +1,39 @@
+int Solution::isPalindrome(string A) 
+{
+    // Ascii value of 'A' = 65 and that of 'a' = 92
+    
+    
+    string s1="";
+    for(auto x : A)
+    {
+        if( x >= 'a'  && x <= 'z')
+        {
+            s1+= x;
+        }
+        
+        if(x >= 'A' && x <= 'Z')
+        {
+            s1+=(x+32);
+        }
+        if(x >= '0' && x <= '9')
+        {
+            s1+=x;
+        }
+    }
+    
+  int l=0;
+  int r = s1.size()-1;
+   
+  while(r >= l)
+  {
+      if(A[l] != A[r])
+      {
+          return 0;
+      }
+       
+      l++;
+      r--;
+  }
+    
+    return 1;
+}
