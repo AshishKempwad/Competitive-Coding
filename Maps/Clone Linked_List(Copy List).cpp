db242aaa9c85fa0a391a79de6dfe8d6f024ed210
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-09 23:18:04 +0530

    Create Clone Linked_List(Copy List).cpp

---
+++ Maps/Clone Linked_List(Copy List).cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+RandomListNode* Solution::copyRandomList(RandomListNode* head) 
+{
+    RandomListNode* curr = head;
+    RandomListNode* clonecurr = NULL;
+    unordered_map<RandomListNode*,RandomListNode*>mp;
+    while(curr!=NULL)
+    {
+        clonecurr = new RandomListNode(curr->label);
+        mp[curr] = clonecurr;
+        curr=curr->next;
+    }
+    
+    curr=head;
+    
+    while(curr!=NULL)
+    {
+        clonecurr = mp[curr];
+        clonecurr->next = mp[curr->next];
+        clonecurr->random = mp[curr->random];
+        curr = curr->next;
+    }
+    curr = head;
+    clonecurr = mp[curr];
+    
+    return clonecurr;
+}
