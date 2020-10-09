44a91a6bd860058e5ec688a38d6d7115f77a919f
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-09 23:59:57 +0530

    Added Optimized method

--- Linked List/Clone a Linked List.cpp
+++ Linked List/Clone a Linked List.cpp
@@ -6,6 +6,13 @@
  *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
  * };
  */
+
+
+//Better Approach
+
+//TC = O(n)
+//SC = O(n)    
+
 RandomListNode* Solution::copyRandomList(RandomListNode* head) 
 {
     RandomListNode* curr = head;
@@ -33,3 +40,65 @@ RandomListNode* Solution::copyRandomList(RandomListNode* head)
     return clonecurr;
 }
 
+-----------------------------------------------------------------------------------------------
+ 
+ //Most optimized Approach   
+    
+//TC = O(n)
+//SC = O(1)    
+    
+    
+RandomListNode* Solution::copyRandomList(RandomListNode* head) 
+{
+   RandomListNode* dummy=NULL;
+   RandomListNode* curr = head;
+   
+   
+   //Take care of next pointers
+   while(curr!=NULL)
+   {
+       dummy = curr->next;
+       curr->next = new RandomListNode(curr->label);
+       curr->next->next = dummy;
+       curr=dummy;
+   }
+   
+   curr = head;
+   //Take care of random pointers
+   while(curr!=NULL)
+   {
+       if(curr->random!=NULL)
+       {
+           curr->next->random = curr->random->next;
+       }
+     
+       curr = curr->next->next;
+   }
+   
+   //segregate original and copy nodes
+   
+   curr = head;
+   dummy = head->next;
+   RandomListNode* temp = dummy;
+  
+   while(curr && dummy)
+   {
+     if(curr->next)
+     {
+         curr->next = curr->next->next;
+     }
+     
+     if(dummy->next)
+     {
+         dummy->next = dummy->next->next;
+     }
+     
+     dummy = dummy->next;
+     curr = curr->next;
+   }
+   return temp;
+   
+   
+   
+  
+}
