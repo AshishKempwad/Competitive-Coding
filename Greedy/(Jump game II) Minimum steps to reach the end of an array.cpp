476a768dc153e9bc4b51ff8d6bd4be4f6fce6221
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-09 18:51:19 +0530

    Asked alot in interviews

--- Greedy/(Jump game II) Minimum steps to reach the end of an array.cpp
+++ Greedy/(Jump game II) Minimum steps to reach the end of an array.cpp
@@ -30,7 +30,7 @@ public:
         int destination = 0;
         int jump = 0;
         
-        for(int i=0;i<nums.size()-1;i++)   // we dont need to consider the last jump.
+        for(int i=0;i<nums.size()-1;i++)   // we dont need to consider the last index.
         {
             destination = max(destination , i+nums[i]);
             if(i == pos)
