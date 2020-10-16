87d396c98abd3d4c443487487941a9804933a915
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-16 18:56:34 +0530

    Update Valid BST from preorder.cpp

--- Trees/Valid BST from preorder.cpp
+++ Trees/Valid BST from preorder.cpp
@@ -1,7 +1,7 @@
 int Solution::solve(vector<int> &A)  //Preorder traversal has been given. 
 {
     //Preorder traversal can NEVER be in the format "2-3-1"
-    // Format(represents) 2(root)-3(left)-1(right).ie. root can never be less than right element.
+    // Format(represents) 2(root)-3(left)-1(right).ie. root can never be greater than right element.
     //We use this and check if "231" pattern is present in the list. If present, then we can
     //say that it is NOT a BST.
     //WE USE STACK FOR THIS PURPOSE
@@ -21,7 +21,7 @@ int Solution::solve(vector<int> &A)  //Preorder traversal has been given.
     
     for(int i=0;i<A.size();i++)
     {
-        if(root > A[i])   //As root can never be less than right element in BST
+        if(root > A[i])   //As root can never be greater than right element in BST
         {
             return 0;
         }
