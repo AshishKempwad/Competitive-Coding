b083fc60162a85a2498e1b252cd291975ac76d7c
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-17 12:36:03 +0530

    Create Find ceil of a key in BST.cpp

---
+++ Trees/Find ceil of a key in BST.cpp
@@ -0,0 +1,25 @@
+int find_ceil(Node* root, int key)
+{
+    int ceil = -1;
+    
+    while(root)
+    {
+       if(root->data == key)
+       {  
+          ceil = root->data;
+          return ceil;
+       }
+       
+       if(root->data > key)
+       {
+          ceil = root->data;
+          root=root->left;
+       }
+       else
+       {  
+          root=root->right;
+       }
+    }
+
+return ceil;
+}
