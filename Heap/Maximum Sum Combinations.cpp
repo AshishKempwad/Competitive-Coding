7ec26d32f8933bfb0da672f1d70f83dbae6cae6a
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-27 01:26:21 +0530

    Update Maximum Sum Combinations.cpp

--- Heap/Maximum Sum Combinations.cpp
+++ Heap/Maximum Sum Combinations.cpp
@@ -72,16 +72,16 @@ Output 1:
 vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) 
 {
      vector<int>res;
-    priority_queue<tuple<int, int, int>>q;
+    priority_queue<tuple<int, int, int>>maxheap;
     sort(A.rbegin(), A.rend()); 
     sort(B.rbegin(), B.rend());
     for(int i=0; i<C; i++) {
-        q.push({A[i]+B[0], i, 0});
+        maxheap.push({A[i]+B[0], i, 0});
     }
     while(res.size()<C) {
-        auto [sum, i, j] = q.top(); q.pop();
+        auto [sum, i, j] = maxheap.top(); maxheap.pop();
         res.push_back(sum);
-        q.push({A[i]+B[j+1], i, j+1});
+        maxheap.push({A[i]+B[j+1], i, j+1});
     }
     return res;
     
