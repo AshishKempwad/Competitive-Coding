3fd1ee394a5a716de6d4044f1104708b1fcaeaa5
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-28 03:37:37 +0530

    Update Word Search Board.cpp

--- Graph_BFS_DFS_Cycle/Word Search Board.cpp
+++ Graph_BFS_DFS_Cycle/Word Search Board.cpp
@@ -1,3 +1,26 @@
+/*
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The cell itself does not count as an adjacent cell.
+The same letter cell may be used more than once.
+
+Example :
+
+Given board =
+
+[
+  ["ABCE"],
+  ["SFCS"],
+  ["ADEE"]
+]
+word = "ABCCED", -> returns 1,
+word = "SEE", -> returns 1,
+word = "ABCB", -> returns 1,
+word = "ABFSAB" -> returns 1
+word = "ABCD" -> returns 0
+Note that 1 corresponds to true, and 0 corresponds to false.
+*/
+
 int dx[4]={-1,0,1,0};
 int dy[4]={0,1,0,-1};
 
