cc42af20b34077abcdb26ca83eafd08db638e998
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-28 04:06:54 +0530

    Update Knight On Chess Board.cpp

--- Graph_BFS_DFS_Cycle/Knight On Chess Board.cpp
+++ Graph_BFS_DFS_Cycle/Knight On Chess Board.cpp
@@ -1,3 +1,34 @@
+/*
+Given any source point, (C, D) and destination point, (E, F) on a chess board, we need to find whether Knight can move to the destination or not.
+
+Knight's movements on a chess board
+
+The above figure details the movements for a knight ( 8 possibilities ).
+
+If yes, then what would be the minimum number of steps for the knight to move to the said point.
+If knight can not move from the source point to the destination point, then return -1.
+
+Note: A knight cannot go out of the board.
+
+
+
+Input Format:
+
+The first argument of input contains an integer A.
+The second argument of input contains an integer B.
+    => The chessboard is of size A x B.
+The third argument of input contains an integer C.
+The fourth argument of input contains an integer D.
+    => The Knight is initially at position (C, D).
+The fifth argument of input contains an integer E.
+The sixth argument of input contains an integer F.
+    => The Knight wants to reach position (E, F).
+Output Format:
+
+If it is possible to reach the destination point, return the minimum number of moves.
+Else return -1.
+
+*/
 int dx[8] = {2,1,-1,-2,-2,-1,1,2};
 int dy[8] = {1,2,2,1,-1,-2,-2,-1};
 
