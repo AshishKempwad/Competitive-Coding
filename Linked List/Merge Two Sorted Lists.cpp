3ea741c24ee12ec950fb27add265859d33085e97
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-29 01:08:26 +0530

    Update Merge Two Sorted Lists.cpp

--- Linked List/Merge Two Sorted Lists.cpp
+++ Linked List/Merge Two Sorted Lists.cpp
@@ -1,4 +1,4 @@
-?*Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+/*Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
 
 Example 1:
 Input: l1 = [1,2,4], l2 = [1,3,4]
