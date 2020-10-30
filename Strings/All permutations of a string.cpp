9eeed8aa9ef40ba327fe74e6476efe73adc381fd
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-10-31 03:46:34 +0530

    Update All permutations of a string.cpp

--- Strings/All permutations of a string.cpp
+++ Strings/All permutations of a string.cpp
@@ -42,4 +42,18 @@ vcetor<int>permutate_all_strings(string s)
     return res;
         
 }    
-
+--------------------------------------------------------------------------------------------------------------
+    //Using stl
+    
+    void permute(string str)
+{
+    // Sort the string in lexicographically
+    // ascennding order
+    sort(str.begin(), str.end());
+ 
+    // Keep printing next permutation while there
+    // is next permutation
+    do {
+       cout << str << endl;
+    } while (next_permutation(str.begin(), str.end()));
+}
