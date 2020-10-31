52892018b9dfbc6b74ce3b608b696b7d78766db8
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-11-01 02:44:37 +0530

    Update Palindrome String.cpp

--- Strings/Palindrome String.cpp
+++ Strings/Palindrome String.cpp
@@ -1,3 +1,14 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Example:
+
+"A man, a plan, a canal: Panama" is a palindrome.
+
+"race a car" is not a palindrome.
+
+Return 0 / 1 ( 0 for false, 1 for true ) for this problem
+*/
 int Solution::isPalindrome(string A) 
 {
     // Ascii value of 'A' = 65 and that of 'a' = 92
