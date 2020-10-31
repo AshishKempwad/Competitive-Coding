5998a723317f4a905d2c870e3f8862dcecf2b2ed
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-11-01 02:19:04 +0530

    Update Length of the smallest sub-string consisting of maximum distinct characters.cpp

--- Strings/Length of the smallest sub-string consisting of maximum distinct characters.cpp
+++ Strings/Length of the smallest sub-string consisting of maximum distinct characters.cpp
@@ -81,3 +81,41 @@ int main()
             "characters : " << len; 
     return 0; 
 }  
+
+--------------------------------------------------------------------------------------------------------
+      
+  //TC=O(n^2)
+      
+  string findSubString(string str) 
+{ 
+    int n = str.length(); 
+  
+    // Count all distinct characters. 
+    int dist_count = 0; 
+    unordered_map<int, int> hash_map; 
+    for (int i = 0; i < n; i++) { 
+        hash_map[str[i]]++; 
+    } 
+  
+    dist_count = hash_map.size(); 
+    int size = INT_MAX; 
+    string res; 
+    // Now follow the algorithm discussed in below 
+    for (int i = 0; i < n; i++) { 
+        int count = 0; 
+        int visited[256] = { 0 }; 
+        string sub_str = ""; 
+        for (int j = i; j < n; j++) { 
+            if (visited[str[j]] == 0) { 
+                count++; 
+                visited[str[j]] = 1; 
+            } 
+            sub_str += str[j]; 
+            if (count == dist_count) 
+                break; 
+        } 
+        if (sub_str.length() < size && count == dist_count) 
+            res = sub_str; 
+    } 
+    return res; 
+}     
