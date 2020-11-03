28d877d46699cf78a6982f0610dd24a8259d5902
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-11-04 03:31:49 +0530

    Update Valid Suduko.cpp

--- Maps/Valid Suduko.cpp
+++ Maps/Valid Suduko.cpp
@@ -1,24 +1,26 @@
  //IMPORTANT:
 
-//map<int,int>mp[10] means vector<map<int,int>>mp(10)
-
-
-vector<map<int,int>>row(9);
-        vector<map<int,int>>col(9);
-        vector<map<int,int>>grid(9);
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) 
+    {
+       map<pair<int,int>,int>row;
+       map<pair<int,int>,int>col;
+       map<pair<int,int>,int>grid;
         
         for(int i=0;i<board.size();i++)
         {
-            for(int j=0;j<board[i].size();j++)
+            for(int j=0;j<board[0].size();j++)
             {
-                char res = board[i][j];
+                char res= board[i][j];
                 if(board[i][j]!='.')
                 {
-                    row[i][res]++;
-                    col[j][res]++;
-                    grid[(i/3)*3+(j/3)][res]++;        //(i/3)*3+(j/3) gives the reuired 3*3 grid
-                    
-                    if(row[i][res] > 1 || col[j][res] > 1 || grid[(i/3)*3 + (j/3)][res] > 1)
+                    row[{i,res}]++;
+                    col[{j,res}]++;
+                    int x = (i/3)*3+(j/3);   //to get 3*3 matrix
+                    grid[{x,res}]++;
+                  
+                    if(row[{i,res}] > 1 || col[{j,res}]>1 || grid[{x,res}]>1)
                     {
                         return false;
                     }
@@ -26,3 +28,5 @@ vector<map<int,int>>row(9);
             }
         }
         return true;
+    }
+};
