d1b3c1cac1b96864894473ed6493458bc3a7ecdc
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-11-03 14:42:16 +0530

    Create Factorial Trailing Zeroes.cpp

---
+++ Math/Factorial Trailing Zeroes.cpp
@@ -0,0 +1,31 @@
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+Follow up: Could you write a solution that works in logarithmic time complexity?
+
+Example 1:
+
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+
+Example 2:
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+*/
+
+
+int Solution::trailingZeroes(int A) 
+{
+    
+    //TC=O(logA)
+    
+    int cnt=0;
+    while(A/5 > 0)
+    {
+        cnt+=(A/5);
+        A=A/5;
+    }
+    return cnt;
+}
