022a8afacd6a180e63872100307c373d79cffb7e
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-11-05 04:35:30 +0530

    Create Intersection Of Sorted Arrays.cpp

---
+++ Arrays/Intersection Of Sorted Arrays.cpp
@@ -0,0 +1,51 @@
+/*
+Find the intersection of two sorted arrays.
+OR in other words,
+Given 2 sorted arrays, find all the elements which occur in both the arrays.
+
+Example :
+
+Input : 
+    A : [1 2 3 3 4 5 6]
+    B : [3 3 5]
+
+Output : [3 3 5]
+
+Input : 
+    A : [1 2 3 3 4 5 6]
+    B : [3 5]
+
+Output : [3 5]
+
+*/
+
+
+vector<int> Solution::intersect(const vector<int> &A, const vector<int> &B) 
+{
+    int n=A.size();
+    int m=B.size();
+    
+       vector<int>res;
+      int i=0;
+      int j=0;
+      while(i<n && j<m)
+      {
+          if(A[i]==B[j])
+          {
+              res.push_back(A[i]);
+              i++;
+              j++;
+          }
+          else if(A[i]>B[j])
+          {
+              j++;
+          }
+          else
+          {
+              i++;
+          }
+      }
+      
+      return res;
+    
+}
