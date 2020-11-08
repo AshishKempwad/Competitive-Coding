0d0c60ea319db9ecdfd93b8e1086d8dcda58a9a4
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2020-11-09 02:34:50 +0530

    Update Construct Binary Tree From Inorder And Preorder.cpp

--- Trees/Construct Binary Tree From Inorder And Preorder.cpp
+++ Trees/Construct Binary Tree From Inorder And Preorder.cpp
@@ -40,4 +40,53 @@ TreeNode* Solution::buildTree(vector<int> &preorder, vector<int> &inorder)
     return helper(0,0,inorder.size()-1,preorder,inorder);
 }
 
-
+-----------------------------------------------------------------------------------------------
+ //easy implementation
+ 
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int prestart=0;
+    TreeNode* helper(int instart,int inend, vector<int>& inorder,vector<int>& preorder)
+    {
+        if(instart > inend)
+        {
+            return NULL;
+        }
+        
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        int inorder_index=-1;
+        for(int i=instart;i<=inend;i++)
+        {
+            if(inorder[i]==root->val)
+            {
+                inorder_index=i;
+                break;
+            }
+        }
+        
+        prestart++;
+        
+        root->left = helper(instart,inorder_index-1,inorder,preorder);
+        root->right = helper(inorder_index+1,inend,inorder,preorder);
+        
+        return root;
+    }
+    
+    
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+        return helper(0,inorder.size()-1,inorder,preorder);
+    }
+};
