9ae5c6568a0731936728d5ce6140c61dd424ee8d
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2023-12-29 18:24:28 +0530

    Update Symmetric Binary tree.cpp

--- Trees/Symmetric Binary tree.cpp
+++ Trees/Symmetric Binary tree.cpp
@@ -65,3 +65,40 @@ int Solution::isSymmetric(TreeNode* A)
     
     return 1;
 }
+-----------------------------------------------------------------------------------------
+    //Recursive, using the idea of SameTree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q){
+        if(!p && !q){
+            return true;
+        }
+        if(!p || !q){
+            return false;
+        }
+        if(p->val == q->val && isSameTree(p->left,q->right) && isSameTree(p->right,q->left)){
+            return true;
+        }
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root){
+            return true;
+        }
+        if(isSameTree(root->left, root->right)){
+            return true;
+        }
+        return false;
+    }
+};
