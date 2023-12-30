200c19cc870d4138bc6b4512f6c8b9751dc47644
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2023-12-30 17:27:11 +0530

    Update fibonacciNumbers.cpp

--- DP_NEW/fibonacciNumbers.cpp
+++ DP_NEW/fibonacciNumbers.cpp
@@ -1,3 +1,4 @@
+//Recursion TC = O(2^n)
 class Solution {
 public:
     int fib(int n) {
@@ -6,5 +7,70 @@ public:
         }
 
         return fib(n-1)+fib(n-2);
+    }
+};
+
+//Recursion + Memo
+
+class Solution {
+public:
+    int solve(int n, vector<int> &dp){
+        if(n==0 || n==1){
+            return n;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+    }
+    int fib(int n) {
+        if(n == 0 || n==1){    //Base condition is always the smallest valid value of n
+            return n;   
+        }
+
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
+
+//Bottom UP  TC=O(n), SC=O(n)
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        vector<int>dp(n+1);
+        dp[0]=0;  //dp[i]= ith fib number
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+//Constant Space complexity
+//TC = O(n), SC=O(1)
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+
+        int c;
+        int a=0,b=1;
+
+        for(int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
     }
 };
