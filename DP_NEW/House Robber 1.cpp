1ade5f4b6985a96bd6e3b883f76bb8d475e686c1
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2023-12-30 18:34:05 +0530

    Update House Robber 1.cpp

--- DP_NEW/House Robber 1.cpp
+++ DP_NEW/House Robber 1.cpp
@@ -40,3 +40,35 @@ public:
         return solve(nums,0,n);
     }
 };
+
+//Bottom UP
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n==1){
+            return nums[0];
+        }
+        vector<int>t(n+1,0);
+
+        //t[i] = Max stolen money till i'th house
+
+        //If no house : i=0
+        t[0]=0;
+
+        //1 house : i =1
+        t[1]=nums[0];
+
+        for(int i=2;i<=n;i++){
+            int steal = nums[i-1] + t[i-2];  
+            int skip = t[i-1]; //Profit till i-1'th house
+
+            t[i] = max(steal,skip);
+        }
+
+        return t[n]
+;
+    }
+};
+
