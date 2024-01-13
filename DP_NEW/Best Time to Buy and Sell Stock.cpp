7bcab4aaa478dd7513d7e2b0565f13f138da9d85
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2024-01-13 16:56:34 +0530

    Best Time to Buy and Sell Stock

---
+++ DP_NEW/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,62 @@
+//Recusrion + Memo
+class Solution {
+public:
+
+    /*We can have only 1 buy and 1 sell order. k represnts maximum buy-sell you can do*/
+    int dp[100001][2];
+    int solve(vector<int>& prices, int idx, int k, int n, bool buyFlag){
+        if(idx >= n || k <= 0){
+            return 0;
+        }
+
+        if(dp[idx][buyFlag] != -1){
+            return dp[idx][buyFlag];
+        }
+
+        int profit = 0;
+
+        if(buyFlag){
+            int take = solve(prices, idx+1, k, n, false) - prices[idx];
+
+            int notTake = solve(prices, idx+1, k, n, true);
+
+            profit = max({profit, take, notTake});
+
+        }else{
+            int sell = prices[idx] + solve(prices, idx+1, k-1, n, true);
+
+            int notSell = solve(prices, idx+1, k, n, false);
+
+            profit = max({profit, sell, notSell}); 
+        }
+
+        return dp[idx][buyFlag] = profit;
+    }
+
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(prices, 0, 1, n, true);
+    }
+};
+
+
+//Optimal Approach
+TC =O(n)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxProfit = 0;
+        int minPrice = INT_MAX;
+
+        for(int i=0;i<n;i++){
+            minPrice = min(prices[i], minPrice);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
