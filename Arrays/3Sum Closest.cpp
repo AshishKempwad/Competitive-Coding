032f217a20e1f03a4cc00560685e265718ae62e7
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2024-10-21 20:34:56 +0530

    Update 3Sum Closest.cpp

--- Arrays/3Sum Closest.cpp
+++ Arrays/3Sum Closest.cpp
@@ -51,3 +51,36 @@ public:
         return ans;
     }
 };
+
+
+########################################JAVA#########################################
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
+        int closestNumber = 100000;
+
+        Arrays.sort(nums);
+
+        for(int i=0;i<n-2;i++){
+            int j=i+1;
+            int k=n-1;
+
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+
+                if(Math.abs(sum-target) < Math.abs(closestNumber-target)){
+                    closestNumber = sum;
+                }
+
+                if(sum < target){
+                    j++;
+                }else{
+                    k--;
+                }
+            }
+        }
+
+        return closestNumber;
+    }
+}
