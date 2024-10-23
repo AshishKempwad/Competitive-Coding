fc0886fd5ca5771f00f5beccfd93b69584016f70
Ashish Kempwad <54923496+AshishKempwad@users.noreply.github.com>
2024-10-23 17:33:50 +0530

    Update Balance a Binary Search Tree.cpp

--- Trees/Balance a Binary Search Tree.cpp
+++ Trees/Balance a Binary Search Tree.cpp
@@ -53,3 +53,58 @@ public:
         
     }
 };
+
+
+#################################################JAVA########################################
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private void inorderTraversal(TreeNode root,List<Integer>inorder){
+        if(root == null){
+            return;
+        }
+        inorderTraversal(root.left,inorder);
+        inorder.add(root.val);
+        inorderTraversal(root.right,inorder);
+
+    } 
+
+    private TreeNode constructbalanceBST(List<Integer>inorder,int left, int right){
+        if(left > right){
+            return null;
+        }
+
+        int mid = left + (right - left)/2;
+
+        TreeNode root = new TreeNode(inorder.get(mid));
+        root.left = constructbalanceBST(inorder,left,mid-1);
+        root.right = constructbalanceBST(inorder,mid+1,right);
+
+        return root;
+
+    }
+
+    public TreeNode balanceBST(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+
+        List<Integer> inorder = new ArrayList<>();
+        inorderTraversal(root,inorder);
+        return constructbalanceBST(inorder,0,inorder.size()-1);
+    }
+}
